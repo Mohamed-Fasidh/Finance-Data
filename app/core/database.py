a2806b23d6c8d71e89ddded2b969c87dbb9a81c5
Mohamed-Fasidh <21uad051@kamarajengg.edu.in>
2026-04-03 07:25:57 +0530

    Intial Commit

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
-DATABASE_URL = "postgresql://postgres:fasidh786@localhost:5432/finance_db"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+if not DATABASE_URL:
+    DATABASE_URL ="postgresql://postgres:fasidh786@localhost:5432/finance_db"
+
+if DATABASE_URL and DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True
+)
+
 SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
